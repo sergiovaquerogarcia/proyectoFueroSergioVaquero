84621942da1e4bc26bcdc80c33502eb09ae3c8c9
sergiovaquerogarcia <73602886+sergiovaquerogarcia@users.noreply.github.com>
2020-11-04 12:01:31 +0100

    Comenzamos con el Repositorio

---
+++ src/proyectofuturo/ProyectoFuturo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofuturo;
+
+/**
+ *
+ * @author milar
+ */
+public class ProyectoFuturo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String vere;
+        
+        vere=veredicto(args[1], args[2]);
+        System.out.println("Estudiante " + args[0] + " : " + vere);
+    }
+    
+    static String veredicto(String valor1, String valor2) {
+        String vere;
+        
+        if (valor1.equals(valor2)) {
+            vere = valor1 +" horas creo que no es suficiente para " + valor1 + " Modulos";
+        }
+        else if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+            vere= "PERFECTO. Dedicas " + valor2 + " horas para " + valor1 + " Modulos. Sigue así APROBARAS";
+        }
+        else {
+            vere= "MUY MAL. Así no vas a aprobar estudiando " + valor2 + " horas para " + valor1 + " Modulos";
+        }
+        return (vere);
+    }
+}
